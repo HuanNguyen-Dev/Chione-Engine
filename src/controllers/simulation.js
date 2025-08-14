084f368c7c32b9353ad057b65e2d327ab494653f
HuanNguyen-Dev <hnguy138@outlook.com>
2025-08-15 00:16:19 +1000

    just initialized values in the table

--- src/controllers/simulation.js
+++ src/controllers/simulation.js
@@ -6,6 +6,7 @@ exports.falling_snow = (req, res) => {
     try {
         if (!wind_speed) wind_speed = 0;
         if (!wind_dir) wind_dir = null;
+        console.log("------------:>");
         // do required checks for valid wind speed and wind dir ---------->
         const falling_snow_coords = falling_snow(initial_state, steps, height,wind_speed, wind_dir, min_neighbour, max_neighbour);
         return res.status(200).json(falling_snow_coords)
