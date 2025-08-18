3353686dd14ef7b203db02647caa1b53163d144a
HuanNguyen-Dev <hnguy138@outlook.com>
2025-08-19 00:12:40 +1000

    added scp comment in app.js, changed login_message into  register_message for register.html, removed trailing , in creation of user in db.js. added in a docker-compose file

--- src/db.js
+++ src/db.js
@@ -18,7 +18,7 @@ const pool = mariadb.createPool({
       CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
       );
     `);
   } catch (err) {
