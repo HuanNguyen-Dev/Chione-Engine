42f26c46f1dc4793b9368112cdbd02d59c7ced2b
HuanNguyen-Dev <hnguy138@outlook.com>
2025-08-30 21:50:36 +1000

    made endpoints for simulation for logical

--- src/controllers/simulation.js
+++ src/controllers/simulation.js
@@ -8,33 +8,12 @@ exports.falling_snow = (req, res) => {
     try {
         if (!wind_speed) wind_speed = 0;
         if (!wind_dir) wind_dir = null;
-        // console.log({
-        //     initial_state,
-        //     steps,
-        //     height,
-        //     wind_speed,
-        //     wind_dir,
-        //     min_neighbour,
-        //     max_neighbour
-        // });
-        // do required checks for valid wind speed and wind dir ---------->
         const falling_snow_coords = falling_snow(initial_state, steps, height, wind_speed, wind_dir, min_neighbour, max_neighbour);
         return res.status(200).json(falling_snow_coords)
     } catch (error) {
         return res.status(400).json({ error: `Something went wrong: ${error.message}` })
     }
 }
-
-// exports.cellular_automata = (req, res) => {
-//     const { initial_state, min_neighbour, max_neighbour, timeframe } = req.body;
-//     if (!initial_state || !min_neighbour || !max_neighbour || !timeframe) return res.status(422).json({ error: "Please enter in a valid value" })
-//     try {
-//         const cellula_automata_configs = cellula_automata(initial_state, min_neighbour, max_neighbour, timeframe)
-//         return res.status(200).json({ data: cellula_automata_configs })
-//     } catch (error) {
-//         return res.status(400).json({ error: `Something went wrong: ${error.message}` })
-//     }
-// }
 
 // chatgpt
 exports.falling_snow_video = async (req, res) => {
--- src/routes/simulation.js
+++ src/routes/simulation.js
@@ -5,14 +5,14 @@ const { authenticateToken } = require('../middleware/jwt');
 const {falling_snow,falling_snow_video, showRenderPage,save_falling_snow_video} = require('../controllers/simulation');
 
 router.post('/falling_snow_calculation', falling_snow);
-router.get("/cloud_simulation", (req, res) => {
+router.get("/falling_snow_page", (req, res) => {
    res.sendFile(path.join(__dirname,"..", '..',"public", "cloud.html"));
 });
 router.post('/falling_snow_video', falling_snow_video);
 router.post('/save_falling_snow_video',save_falling_snow_video);
 
 
-router.get("/3d_simulation", authenticateToken,showRenderPage);
+router.get("/3d_simulation_page", authenticateToken,showRenderPage);
 
 
 module.exports = router;
